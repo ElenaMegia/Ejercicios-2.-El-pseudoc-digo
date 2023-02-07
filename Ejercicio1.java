d92f161efd5ea6f56ab0ee4105a4881a36e09e5c
ElenaMegia <emegican@myuax.com>
2023-02-07 12:34:51 +0100

    Terminado

---
+++ Ejercicio1.java
@@ -0,0 +1,28 @@
+//¿Cuáles serán los valores de las variables a y b después de cada línea del siguiente algoritmo?
+//
+//Algo ValoresDeLasVariables
+//variable a, b: entero
+//Inicio
+//   a <- 3
+//   escribir("a = " & a)
+//   b<-a+5
+//   escribir("a = " & a & " y b = " & b)
+//   a<-7
+//   escribir("a = " & a & " y b = " & b)
+//Fin
+
+
+public class Ejercicio1 {
+    //escribir en java el codigo del algoritmo
+    public static void main(String[] args) {
+        int a, b;
+        a = 3;
+        System.out.println("a = " + a);
+        b = a + 5;
+        System.out.println("a = " + a + " y b = " + b);
+        a = 7;
+        System.out.println("a = " + a + " y b = " + b);
+    }
+
+
+}
---
+++ Ejercicio4.java
@@ -0,0 +1,25 @@
+//Escriba un algoritmo que calcule la velocidad media de desplazamiento del usuario.
+//A continuación, se muestra un ejemplo de posibles visualizaciones y entradas durante una ejecución del algoritmo (los valores introducidos por el usuario están escritos en negrita y cursiva):
+//Introduzca la distancia recorrida (km).
+//370
+//Introduzca el tiempo del recorrido (min).
+//240
+//Se ha desplazado a una velocidad de 92,5 km/h.
+
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    static Scanner sc = new Scanner(System.in);
+    //Escriba un algoritmo que calcule la velocidad media de desplazamiento del usuario.
+    public static void main(String[] args) {
+        double distancia, tiempo, velocidad;
+        System.out.println("Introduzca la distancia recorrida (km).");
+        distancia = sc.nextDouble();
+                System.out.println("Introduzca el tiempo del recorrido (min).");
+        tiempo = sc.nextDouble();
+        tiempo = tiempo / 60;
+        velocidad = distancia / tiempo;
+        System.out.println("Se ha desplazado a una velocidad de " + velocidad + " km/h.");
+    }
+}
